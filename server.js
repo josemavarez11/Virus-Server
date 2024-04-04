5e74288ca492669a587076bd5e338e59087b9c87
josemavarez11 <josemmr311@gmail.com>
2024-04-05 01:04:20 +0200

    fixing socket server

--- server.js
+++ server.js
@@ -2,15 +2,16 @@ import express from 'express';
 import http from 'http';
 import { Server as SocketIO } from 'socket.io';
 import fs from 'fs';
+import { fileURLToPath } from 'url';
 import path from 'path';
 
 const app = express();
 const server = http.createServer(app);
 const io = new SocketIO(server);
 
-const UPLOADS_FOLDER = path.join(path.dirname(import.meta.url), 'uploads');
-
-app.use(express.static(path.join(path.dirname(import.meta.url), 'public')));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const UPLOADS_FOLDER = path.join(__dirname, 'uploads');
 
 io.on('connection', (socket) => {
     console.log('Client connected');
@@ -25,17 +26,12 @@ io.on('connection', (socket) => {
     });
 
     socket.on('file', (data) => {
-        const { nombre, tipo, contenido } = data;
-        const filePath = path.join(UPLOADS_FOLDER, nombre);
+        const { name, content } = data;
 
-        fs.writeFile(filePath, contenido, 'binary', (err) => {
-            if (err) {
-                console.error('Error trying to save the file:', err);
-                socket.emit('error', { mensaje: 'Error trying to save the file at server' });
-            } else {
-                console.log('File saved succesfully:', nombre);
-                socket.emit('success', { mensaje: 'File saved succesfully' });
-            }
+        const filePath = path.join(UPLOADS_FOLDER, name);
+        fs.writeFile(filePath, content, (err) => {
+            if (err) console.error('Error saving file:', err);
+            else socket.emit('file', { mensaje: 'File saved successfully' });
         });
     });
 });
